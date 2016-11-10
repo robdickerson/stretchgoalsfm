55cff94c36c3a2e34b154987db4dd07bf848002f
Robert Dickerson <robert@aurovo.com>
2016-11-10 11:55:31 -0500

    Updates

---
+++ Gulpfile.js
@@ -0,0 +1,53 @@
+//Init variables
+var gulp = require('gulp'),
+
+    firstRun = true,
+    cleanOnce = true,
+
+    // Load all the gulp Plugins
+    plugins = require('gulp-load-plugins')(
+    {
+        pattern: ['gulp-*', 'gulp.*', 'karma'],
+        replaceString: /\bgulp[\-.]/
+    });
+
+// Compile SASS to CSS
+gulp.task('styles', ['clean'], function () {
+
+    return gulp.src('assets/sass/*.scss')
+        .pipe(plugins.sass().on('error', plugins.sass.logError))
+
+        // Init the sourcemaps
+        .pipe(plugins.sourcemaps.init())
+
+        // Concat the files
+        .pipe(plugins.concat('site.css'))
+
+        // Finalize the sourcemaps
+        .pipe(plugins.sourcemaps.write('./'))
+
+        //.pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(gulp.dest('assets/css'))
+
+});
+
+// // Clean the build directories
+gulp.task('clean', function () {
+
+    if (cleanOnce) {
+
+        cleanOnce = false;
+
+        return gulp.src([
+            'assets/css/*'
+        ]).pipe(plugins.clean());
+
+    }
+
+});
+
+
+gulp.task('default', [], function () {
+    gulp.watch('assets/**/*.scss', ['styles']);
+});
+
